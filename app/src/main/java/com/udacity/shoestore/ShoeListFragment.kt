cb249b0ceecc0b147ea465705caf01a9c652b1d2
wade-profe <wade.profe@gmail.com>
2023-06-28 20:29:25 +0200

    Added programmatic shoe list view creation

--- app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.marginTop
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.udacity.shoestore.databinding.ShoeListFragmentBinding
+import com.udacity.shoestore.databinding.ShoeListItemBinding
 
 class ShoeListFragment: Fragment() {
 
     lateinit var shoeListBinding: ShoeListFragmentBinding
+    val shoeListViewModel:ShoeListViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,8 +22,16 @@ class ShoeListFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         shoeListBinding = DataBindingUtil.inflate(inflater, R.layout.shoe_list_fragment, container, false)
-        return shoeListBinding.root
 
+        shoeListViewModel.shoeList.observe(viewLifecycleOwner) {shoeList ->
+            shoeList.forEach {shoe ->
+                val shoeListItemBinding = ShoeListItemBinding.inflate(layoutInflater, null, false)
+                shoeListItemBinding.shoe = shoe
+                shoeListBinding.shoeListLinearLayout.addView(shoeListItemBinding.root)
+            }
+        }
+
+        return shoeListBinding.root
 
     }
 }
--- app/src/main/java/com/udacity/shoestore/ShoeListViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeListViewModel.kt
@@ -8,7 +8,11 @@ import com.udacity.shoestore.models.Shoe
 class ShoeListViewModel: ViewModel() {
 
     private val _shoeList = MutableLiveData<List<Shoe>>().apply { value = mutableListOf(Shoe("Test1", 9.0, "Nike", "a Nike shoe"),
-        Shoe("Test2", 5.0, "Adidas", "an Adidas shoe"), Shoe("Test3", 7.5, "Puma", "a Puma shoe")) }
+        Shoe("Test2", 5.0, "Adidas", "an Adidas shoe"), Shoe("Test3", 7.5, "Puma", "a Puma shoe"),
+        Shoe("Test3", 7.5, "Puma", "a Puma shoe"),
+        Shoe("Test4", 7.5, "Puma", "a Puma shoe"),
+        Shoe("Test5", 7.5, "Puma", "a Puma shoe"),
+        Shoe("Test6", 7.5, "Puma", "a Puma shoe"))}
 
     val shoeList: LiveData<List<Shoe>>
         get() = _shoeList
